a743f60d02a22742fe8637e9f6734cf867af3d23
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-10-12 15:13:46 +0300

    Initial Commit

---
+++ SwiftUICombineTodoApp/SwiftUICombineTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineTodoAppApp.swift
+//  SwiftUICombineTodoApp
+//
+//  Created by Yunus Emre Taşdemir on 12.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
